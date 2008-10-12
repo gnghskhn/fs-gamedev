d2187eea8dc61074683f38d2908490baf03ce364
Johann.Deneux <Johann.Deneux@c9452bf0-96f5-11dd-9ecb-1b8c227eb0e4>
2008-10-12 12:52:07 +0000

    Cleaned up a bit. Added runge-kutta.

--- Tutorial009/Program009.fs
+++ Tutorial009/Program009.fs
@@ -27,32 +27,13 @@ type ps<'num> = struct
     new(pos, speed) = { pos = pos; speed = speed }
 end
     
-type VecN =
-    One of float
-    | Two of float*float
-    | Three of float*float*float
-with
-    static member inline plus (a: VecN) (b: VecN) =
-        match (a, b) with
-            | (One(v), One(w)) -> One(v+w)
-            | (Two(v0, v1), Two(w0, w1)) -> Two(v0 + w0, v1 + w1)
-            | (Three(v0, v1, v2), Three(w0, w1, w2)) -> Three(v0 + w0, v1 + w1, v2 + w2)
-            | _ -> failwith "Size mismatch"
-
-    static member inline scale (k: float) (a: VecN) =
-        match a with
-            | One(v) -> One(k * v)
-            | Two(v0, v1) -> Two(k * v0, k * v1)
-            | Three(v0, v1, v2)  -> Three(k * v0, k * v1, k * v2)
-
-
 type Vec1d = struct
     val x: float
     
     new (x) = { x = x }
     
-    static member inline plus (a: Vec1d) (b: Vec1d) = Vec1d(a.x + b.x)
-    static member inline scale (k: float) (a: Vec1d) = Vec1d(k * a.x)
+    static member plus (a: Vec1d) (b: Vec1d) = Vec1d(a.x + b.x)
+    static member scale (k: float) (a: Vec1d) = Vec1d(k * a.x)
 end
 
 
@@ -62,8 +43,8 @@ type Vec2d = struct
     
     new (x, y) = { x = x; y = y }
     
-    static member inline plus (a: Vec2d) (b: Vec2d) = Vec2d(a.x + b.x, a.y + b.y)
-    static member inline scale (k: float) (a: Vec2d) = Vec2d(k * a.x, k * a.y)
+    static member plus (a: Vec2d) (b: Vec2d) = Vec2d(a.x + b.x, a.y + b.y)
+    static member scale (k: float) (a: Vec2d) = Vec2d(k * a.x, k * a.y)
 end
 
 
@@ -74,16 +55,16 @@ type Vec3d = struct
     
     new (x, y, z) = { x = x; y = y; z = z }
     
-    static member inline plus (a: Vec3d) (b: Vec3d) = Vec3d(a.x + b.x, a.y + b.y, a.z + b.z)
-    static member inline scale (k: float) (a: Vec3d) = Vec3d(k * a.x, k * a.y, k * a.z)
+    static member plus (a: Vec3d) (b: Vec3d) = Vec3d(a.x + b.x, a.y + b.y, a.z + b.z)
+    static member scale (k: float) (a: Vec3d) = Vec3d(k * a.x, k * a.y, k * a.z)
 end
 
 
 type CVec1d (x: float) = class
     member v.X = x
 
-    static member inline plus (a: CVec1d) (b: CVec1d) = CVec1d(a.X + b.X)
-    static member inline scale (k: float) (a: CVec1d) = CVec1d(k * a.X)
+    static member plus (a: CVec1d) (b: CVec1d) = CVec1d(a.X + b.X)
+    static member scale (k: float) (a: CVec1d) = CVec1d(k * a.X)
 end
 
 
@@ -91,8 +72,8 @@ type CVec2d (x: float, y:float) = class
     member v.X = x
     member v.Y = y
 
-    static member inline plus (a: CVec2d) (b: CVec2d) = CVec2d(a.X + b.X, a.Y + b.Y)
-    static member inline scale (k: float) (a: CVec2d) = CVec2d(k * a.X, k * a.Y)
+    static member plus (a: CVec2d) (b: CVec2d) = CVec2d(a.X + b.X, a.Y + b.Y)
+    static member scale (k: float) (a: CVec2d) = CVec2d(k * a.X, k * a.Y)
 end
 
 
@@ -101,8 +82,8 @@ type CVec3d (x: float, y: float, z: float) = class
     member v.Y = y
     member v.Z = z
     
-    static member inline plus (a: CVec3d) (b: CVec3d) = CVec3d(a.X + b.X, a.Y + b.Y, a.Z + b.Z)
-    static member inline scale (k: float) (a: CVec3d) = CVec3d(k * a.X, k * a.Y, k * a.Z)
+    static member plus (a: CVec3d) (b: CVec3d) = CVec3d(a.X + b.X, a.Y + b.Y, a.Z + b.Z)
+    static member scale (k: float) (a: CVec3d) = CVec3d(k * a.X, k * a.Y, k * a.Z)
 end
 
     
@@ -111,26 +92,57 @@ let gravity =
 
 
 let inline spring (pos: ^vec) =
-    let (*) k x = (^vec: (static member inline scale: float -> ^vec -> ^vec) k) x
+    let (*) k x = (^vec: (static member scale: float -> ^vec -> ^vec) k) x
     let k = 100.0
     -k * pos
 
 
 let inline drag k (pos: ^pos) (speed: ^pos) =
-    let (*) k x = (^vec: (static member inline scale: float -> ^vec -> ^vec) k) x
+    let (*) k x = (^vec: (static member scale: float -> ^vec -> ^vec) k) x
     -k * speed
 
 
 let inline euler_implicit accel_func (pos: ^vec) (speed: ^vec) delta =
-    let (+) x y = (^vec: (static member inline plus: ^vec -> ^vec -> ^vec) x) y
-    let (*) k x = (^vec: (static member inline scale: float -> ^vec -> ^vec) k) x
+    let (+) x y = (^vec: (static member plus: ^vec -> ^vec -> ^vec) x) y
+    let (*) k x = (^vec: (static member scale: float -> ^vec -> ^vec) k) x
     
     let speed2 = speed + (delta * (accel_func pos speed))    
     let pos2 = pos + (delta * speed2)        
     ps<_>(pos2, speed2)
 
 
-let inline simulate intg_func t0 t_fin delta pos0 speed0 =
+let adapt (f: 'num -> 'num -> 'num) pos speed = 
+    let accel = f pos speed
+    speed, accel
+
+
+let inline runge_kutta_4 deriv_func (pos: ^vec) (speed:  ^vec) delta =
+    let add x y = (^vec: (static member plus: ^vec -> ^vec -> ^vec) x) y
+    let scale k x = (^vec: (static member scale: float -> ^vec -> ^vec) k) x
+    
+    let extrapolate x delta dx = 
+        add x (scale delta dx)
+
+    let half_delta = 0.5 * delta
+         
+    let s1, a1 = deriv_func pos speed
+    let s2, a2 = deriv_func (extrapolate pos half_delta s1) (extrapolate speed half_delta a1)
+    let s3, a3 = deriv_func (extrapolate pos half_delta s2) (extrapolate speed half_delta a2)
+    let s4, a4 = deriv_func (extrapolate pos delta s3)      (extrapolate speed delta a3)
+
+    let sixth_delta = delta / 6.0    
+
+    let update old v1 v2 v3 v4 =
+        let (*) k v = scale k v
+        let (+) u v = add u v
+        old + sixth_delta * (v1 + 2.0 * (v2 + v3) + v4)
+                             
+    let pos1 = update pos s1 s2 s3 s4
+    let speed1 = update speed a1 a2 a3 a4
+    ps<_>(pos1, speed1)
+
+
+let simulate intg_func t0 t_fin delta pos0 speed0 =
     let oc = OptimizedClosures.FastFunc3<_,_,_,_>.Adapt(intg_func)
     let rec repeat t0 pos0 speed0 =
         if t0 > t_fin then (pos0, speed0)
@@ -142,8 +154,8 @@ let inline simulate intg_func t0 t_fin delta pos0 speed0 =
     
 
 let inline accel (up: ^vec) (pos: ^vec) (speed: ^vec) =
-    let (+) x y = (^vec: (static member inline plus: ^vec -> ^vec -> ^vec) x) y
-    let (*) k x = (^vec: (static member inline scale: float -> ^vec -> ^vec) k) x
+    let (+) x y = (^vec: (static member plus: ^vec -> ^vec -> ^vec) x) y
+    let (*) k x = (^vec: (static member scale: float -> ^vec -> ^vec) k) x
     (drag 1.0 pos speed) + (spring pos) + (gravity * up)
 
 
@@ -175,59 +187,48 @@ let run euler_func s0 =
 
 let n_bodies = 100
 
+let accel_Vec1d pos speed = accel (Vec1d(1.0)) pos speed
+let accel_Vec1d' pos speed = (adapt accel_Vec1d) pos speed
 let s0_Vec1d = [ for s in 1..n_bodies -> (Vec1d(float s), Vec1d(0.0)) ]
-let ef_Vec1d = euler_implicit (accel (Vec1d(1.0)))
-
-let s0_Vec2d = [ for s in 1..n_bodies -> (Vec2d(1.0, float s), Vec2d(0.0, 0.0)) ]
-let ef_Vec2d = euler_implicit (accel (Vec2d(0.0, 1.0)))
-
-let s0_Vec3d = [ for s in 1..n_bodies -> (Vec3d(1.0, float s, -1.0), Vec3d(0.0, 0.0, 0.0)) ]
-let ef_Vec3d = euler_implicit (accel (Vec3d(0.0, 1.0, 0.0)))
-
-let s0_CVec1d = [ for s in 1..n_bodies -> (CVec1d(float s), CVec1d(0.0)) ]
-let ef_CVec1d = euler_implicit (accel (CVec1d(1.0)))
-
-let s0_CVec2d = [ for s in 1..n_bodies -> (CVec2d(1.0, float s), CVec2d(0.0, 0.0)) ]
-let ef_CVec2d = euler_implicit (accel (CVec2d(0.0, 1.0)))
-
-let s0_CVec3d = [ for s in 1..n_bodies -> (CVec3d(1.0, float s, -1.0), CVec3d(0.0, 0.0, 0.0)) ]
-let ef_CVec3d = euler_implicit (accel (CVec3d(0.0, 1.0, 0.0)))
-
-(*
-let s0_One = [ for s in 1..n_bodies -> (One(float s), One(0.0)) ]
-let ef_One = euler_implicit (accel (One(1.0)))
-
-let s0_Two = [ for s in 1..n_bodies -> (Two(float s, 1.0), Two(0.0, 0.0)) ]
-let ef_Two = euler_implicit (accel (Two(0.0, 1.0)))
-
-let s0_Three = [ for s in 1..n_bodies -> (Three(float s, 1.0, -1.0), Three(0.0, 0.0, 0.0)) ]
-let ef_Three = euler_implicit (accel (Three(0.0, 1.0, 0.0)))
-*)
+let ef_Vec1d = euler_implicit (accel_Vec1d)
+let rk4_Vec1d = runge_kutta_4 (accel_Vec1d')
 printfn "1D (struct)"
 run ef_Vec1d s0_Vec1d
+run rk4_Vec1d s0_Vec1d
 
+let accel_Vec2d pos speed = accel (Vec2d(0.0, 1.0)) pos speed
+let accel_Vec2d' pos speed = (adapt accel_Vec2d) pos speed
+let s0_Vec2d = [ for s in 1..n_bodies -> (Vec2d(1.0, float s), Vec2d(0.0, 0.0)) ]
+let ef_Vec2d = euler_implicit (accel_Vec2d)
+let rk4_Vec2d = runge_kutta_4 (accel_Vec2d')
 printfn "2D (struct)"
 run ef_Vec2d s0_Vec2d
- 
+run rk4_Vec2d s0_Vec2d
+
+let accel_Vec3d pos speed = accel (Vec3d(0.0, 1.0, 0.0)) pos speed
+let accel_Vec3d' pos speed = (adapt accel_Vec3d) pos speed
+let s0_Vec3d = [ for s in 1..n_bodies -> (Vec3d(1.0, float s, -1.0), Vec3d(0.0, 0.0, 0.0)) ]
+let ef_Vec3d = euler_implicit (accel_Vec3d)
+let rk4_Vec3d = runge_kutta_4 (accel_Vec3d')
 printfn "3D (struct)"
 run ef_Vec3d s0_Vec3d
+run rk4_Vec3d s0_Vec3d
+
 (*
+let s0_CVec1d = [ for s in 1..n_bodies -> (CVec1d(float s), CVec1d(0.0)) ]
+let ef_CVec1d = euler_implicit (accel (CVec1d(1.0)))
 printfn "1D (class)"
 run ef_CVec1d s0_CVec1d
 
+let s0_CVec2d = [ for s in 1..n_bodies -> (CVec2d(1.0, float s), CVec2d(0.0, 0.0)) ]
+let ef_CVec2d = euler_implicit (accel (CVec2d(0.0, 1.0)))
 printfn "2D (class)"
 run ef_CVec2d s0_CVec2d
- 
+
+let s0_CVec3d = [ for s in 1..n_bodies -> (CVec3d(1.0, float s, -1.0), CVec3d(0.0, 0.0, 0.0)) ]
+let ef_CVec3d = euler_implicit (accel (CVec3d(0.0, 1.0, 0.0)))
 printfn "3D (class)"
 run ef_CVec3d s0_CVec3d
+*)
 
-printfn "1D (union)"
-run ef_One s0_One
-
-printfn "2D (union)"
-run ef_Two s0_Two
- 
-printfn "3D (union)"
-run ef_Three s0_Three
-*) 
 ignore (Console.ReadKey(true))
